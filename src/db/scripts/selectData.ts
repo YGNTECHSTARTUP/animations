035db50a4ecd2e4e79db2ef879077cb92a30f1f4
YGN <ygntechstartup@gmail.com>
2024-06-15 12:27:11 +0530

    added the select function

--- src/db/scripts/selectData.ts
+++ src/db/scripts/selectData.ts
@@ -2,5 +2,8 @@ import { db } from "../db";
 import { Data } from "../schema";
 export const selectData = async () => {
     const data = await db.select().from(Data)
+    const randomPokemonID = Math.floor(Math.random() * 898)+2;
+    console.log(randomPokemonID)
     return data
 }
+selectData()
