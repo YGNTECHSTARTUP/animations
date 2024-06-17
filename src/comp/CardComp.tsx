88bbc8e9f75f665f98d1eae732ad1f6b73b8433d
YGN <ygntechstartup@gmail.com>
2024-06-17 13:50:15 +0530

    changed few things

--- src/comp/CardComp.tsx
+++ src/comp/CardComp.tsx
@@ -1,19 +1,20 @@
-import React from 'react'
 import { Card, CardContent } from '@/components/ui/card'
 import Image from 'next/image'
 import Link from 'next/link'
 import { db } from '@/db/db'
 import { Data } from '@/db/schema'
+export const dynamic ="force-dynamic"
+
 
 // Define the props interface for better type checking
 
-export async function selectData() {
+ async function selectData() {
   const data = await db.select().from(Data)
   return data
 }
-export const dynamic ="force-dynamic"
-const CardComp: React.FC = async() => {
+const CardComp = async() => {
   const data = await selectData()
+  console.log(data)
   return (
     <div className='grid grid-cols-1 gap-10 p-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5'>
       {data.map((product) => (
