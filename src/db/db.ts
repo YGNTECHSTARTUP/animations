44d678906f65b9bb1e59e1841e2c21698c5ae76f
YGN <ygntechstartup@gmail.com>
2024-06-17 13:54:59 +0530

    given the database connection

--- src/db/db.ts
+++ src/db/db.ts
@@ -3,5 +3,5 @@ import * as schema from "./schema"
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 config({path:'.env'})
-export const sql = neon(process.env.DATABASE_URL!)
+export const sql = neon('postgresql://pricify_owner:9En6UpKLejwo@ep-rapid-brook-a5kcbrrt.us-east-2.aws.neon.tech/pricify?sslmode=require')
 export const db = drizzle(sql,{schema})
